fd31a4e422d963981e825137cdc3eac417458b61
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-04 00:38:02 -0300

    adiciona funções para remover e atualizar status de tickets, e ajusta rotas correspondentes

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,6 +1,8 @@
 import { create } from "../controllers/tickets/create.js";
 import { index } from "../controllers/tickets/index.js";
 import { update } from "../controllers/tickets/update.js";
+import { updateStatus } from "../controllers/tickets/updateStatus.js";
+import { remove } from "../controllers/tickets/remove.js";
 
 create;
 
@@ -19,5 +21,15 @@ export const tickets = [
     method: "PUT",
     path: "/tickets/:id",
     controller: update,
+  },
+  {
+    method: "PATCH",
+    path: "/tickets/:id/close",
+    controller: updateStatus,
+  },
+  {
+    method: "DELETE",
+    path: "/tickets/:id",
+    controller: remove,
   },
 ];
